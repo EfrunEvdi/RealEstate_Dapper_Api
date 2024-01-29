641aea0aaa9632ca901fcff117a12c4ee6c5781b
Efrun Evdi <efruncetkin@gmail.com>
2024-01-29 21:48:41 +0300

    44. Ders Sonu

--- RealEstate_Dapper_Api/Repositories/ProductRepository/ProductRepository.cs
+++ RealEstate_Dapper_Api/Repositories/ProductRepository/ProductRepository.cs
@@ -27,12 +27,36 @@ namespace RealEstate_Dapper_Api.Repositories.ProductRepository
 
         public async Task<List<ResultProductWithCategoryDto>> GetAllProductWithCategoryAsync()
         {
-            string query = "Select ProductID,Title,Price,City,District,Name,CoverImage,Type,Address From Products Inner Join Categories On Products.ProductCategory=Categories.CategoryID";
+            string query = "Select ProductID,Title,Price,City,District,Name,CoverImage,Type,Address,DealOfTheDay From Products Inner Join Categories On Products.ProductCategory=Categories.CategoryID";
 
             using (var connection = _context.CreateConnection())
             {
                 var values = await connection.QueryAsync<ResultProductWithCategoryDto>(query);
                 return values.ToList();
+            }
+        }
+
+        public async void ProductDealOfTheDayStatusChangeToFalse(int id)
+        {
+            string query = "Update Products Set DealOfTheDay=0 Where ProductID=@productID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@productID", id);
+
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public async void ProductDealOfTheDayStatusChangeToTrue(int id)
+        {
+            string query = "Update Products Set DealOfTheDay=1 Where ProductID=@productID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@productID", id);
+
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
             }
         }
     }
