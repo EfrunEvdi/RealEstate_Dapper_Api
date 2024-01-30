a6d93344d17d36b7c01f4ad2f792e12d6ac9e701
Efrun Evdi <efruncetkin@gmail.com>
2024-01-30 23:14:04 +0300

    46. Ders Sonu

---
+++ RealEstate_Dapper_UI/ViewComponents/Dashboard/_DashboardLast5ProductComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace RealEstate_Dapper_UI.ViewComponents.Dashboard
+{
+    public class _DashboardLast5ProductComponentPartial : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
